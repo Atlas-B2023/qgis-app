a41e930711eaf58ddb0bdbf09e9a17fe9f70ff97
WADDAWADDA <76667563+WADDAWADDA@users.noreply.github.com>
2023-10-30 13:36:39 -0400

    Uses metro zips, can select features,  and works in qgis app

--- our_qgis.py
+++ our_qgis.py
@@ -10,9 +10,10 @@ qgs = QgsApplication([], False)
 qgs.initQgis()
 
 # Create a map canvas
-canvas = QgsMapCanvas()
-canvas.setWindowTitle("My QGIS Map")
-canvas.setCanvasColor(Qt.white)
+canvas = iface.mapCanvas()
+# canvas = QgsMapCanvas()
+# canvas.setWindowTitle("My QGIS Map")
+# canvas.setCanvasColor(Qt.white)
 
 # Create a project
 project = QgsProject.instance()
@@ -25,7 +26,7 @@ bridge = QgsLayerTreeMapCanvasBridge(root, canvas)
 # Load all layers
 layers = []
 scriptDirectory = os.path.dirname(os.path.abspath(__file__))
-layersFolder = "layers/qgis_sample_data/shapefiles"
+layersFolder = "layers/Metro_Zips"
 folderDirectory = os.path.join(scriptDirectory, layersFolder)
 
 for filename in os.listdir(folderDirectory):
@@ -35,11 +36,31 @@ for filename in os.listdir(folderDirectory):
     
     if layer.isValid() and filename.endswith(".shp"):
         # Add the layer to the canvas
-        #layers.add(layer)
+        layers.append(layer)
         project.instance().addMapLayer(layer)
         canvas.setExtent(layer.extent())
     elif filename.endswith(".shp"):
         print("Layer failed to load!")
+        
+# Custom map tool for identifying features
+class IdentifyMapTool(QgsMapTool):
+    def __init__(self, canvas, layers):
+        super().__init__(canvas)
+        self.layers = layers
+
+    def canvasReleaseEvent(self, event):
+        for layer in self.layers:
+            identify = QgsMapToolIdentify(canvas)
+            result = identify.identify(event.x(), event.y(), [layer], QgsMapToolIdentify.TopDownStopAtFirst)
+            if result:
+                for feat in result:
+                    field_names = feat.mFeature.fields().names()
+                    info = "\n".join([name + " " + str(feat.mFeature.attribute(name)) for name in field_names])
+                    print(info)
+
+# Set the custom identify map tool
+identifyMapTool = IdentifyMapTool(canvas, layers)
+canvas.setMapTool(identifyMapTool)
         
 # Show the map canvas
 canvas.show()
